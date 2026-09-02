943035fd10958deebb53410daa13b90f5a64b7ca
Andrey Omelyanuk <omelyanukandrey@gmail.com>
2026-09-02 09:33:38 +0300

    Фронт: вкладка Config со вторым топ-меню (Env/Users/Skills/Commands/Agent Set)

---
+++ front/src/pages/app/configUsers.tsx
@@ -0,0 +1,9 @@
+import { PersonnelPanel } from '@/components/settings/PersonnelPanel';
+
+const ConfigUsersPage = () => (
+  <div className="max-w-2xl">
+    <PersonnelPanel />
+  </div>
+);
+
+export default ConfigUsersPage;
